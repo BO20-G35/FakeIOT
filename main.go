63655767577297060bacfe23e7253fa67ef7c669
TobiasTR <tobiast98kontakt@protonmail.com>
2020-02-25 14:15:42 +0100

    på begynt

--- main.go
+++ main.go
@@ -2,52 +2,46 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 )
-
 
 type lock struct {
 	status string
 }
 
-
-var vulnLock = lock{status:string("1")}
+var vulnLock = lock{status: string("1")}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "Verry vulnerable IoT Lock !")
 }
 
-
-func unLock(w http.ResponseWriter, r *http.Request)  {
+func unLock(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("unLock called")
 	vulnLock.status = "0"
 }
 
-
-func lockTheLock(w http.ResponseWriter, r *http.Request)  {
+func lockTheLock(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("lockTheLock called")
 	vulnLock.status = "1"
 }
 
-
-func getStatus(w http.ResponseWriter, r *http.Request)  {
+func getStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("getStatus called")
 
 	_, err := fmt.Fprint(w, vulnLock.status)
-	if err != nil{
+	if err != nil {
 		fmt.Println("ERROR: " + err.Error())
 	}
 }
-
 
 func main() {
 	fmt.Println("Starting fake IOT")
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/status",getStatus).Methods("GET")
-	router.HandleFunc("/status/1",lockTheLock).Methods("GET")
-	router.HandleFunc("/status/0",unLock).Methods("GET")
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", router))
+	router.HandleFunc("/status", getStatus).Methods("GET")
+	router.HandleFunc("/status/1", lockTheLock).Methods("GET")
+	router.HandleFunc("/status/0", unLock).Methods("GET")
+	log.Fatal(http.ListenAndServe("192.168.2.225:8080", router))
 }
---
+++ settings.go
@@ -0,0 +1,13 @@
+package main
+
+import "net"
+
+type settings struct {
+	ip   string
+	port string
+	addr net.Addr
+}
+
+func readConfig() {
+
+}
