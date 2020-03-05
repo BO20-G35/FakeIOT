04417ea13947d709611d6241f3e7352d80796704
TobiasTR <tobiast98kontakt@protonmail.com>
2020-03-05 17:46:20 +0100

    notater

--- xml_read_test.py
+++ xml_read_test.py
@@ -15,3 +15,10 @@ print(items)
 
 process = psutil.Process(os.getpid())
 print(process.memory_info().rss)  # 42MB
+
+
+#tiden det tar å lese inn xml filen linje 9/10
+#vil ta lang tid hvis de har lagd en xml bombe
+#vi kan ta tiden før den bli lest inn så etter 
+#hvis tiden er si 10 sec så har de klart det
+#bakdel er at de må ikke lage en for stor bombe 
